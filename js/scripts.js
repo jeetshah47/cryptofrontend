d723fece59c316f6cf57e8721e8b077aaad1f831
TASK <19125025@nuv.ac.in>
2022-05-04 18:52:52 +0530

    Deploy 1.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-var start_btn = document.getElementById("startup-cost");
+var start_btn_cost = document.getElementById("startup-cost");
+
+start_btn.addEventListener("click", () => {
+  fetch("localhost:3000/start?catch=on").then((response) =>
+    alert(response.message)
+  );
+});
+
+var start_btn_nesw = document.getElementById("startup-nesw");
 
 start_btn.addEventListener("click", () => {
   fetch("localhost:3000/start?catch=on").then((response) =>
