d825c616394a2edde869c231ac76fd7458804d82
TASK <19125025@nuv.ac.in>
2022-05-04 18:49:27 +0530

    Deploy 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var start_btn = document.getElementById("startup");
+var start_btn = document.getElementById("startup-cost");
 
 start_btn.addEventListener("click", () => {
   fetch("localhost:3000/start?catch=on").then((response) =>
